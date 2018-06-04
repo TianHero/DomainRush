b2194cea95997259ceaf50cb8d445af5b7df64cb
zhaotian <1246127205@qq.com>
2018-06-04 10:40:03 +0800

    fixed eu contactType

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 function User() {
-  this.auth_userid = "";//代理账号id
-  this.api_key = "";//api_key
+  this.auth_userid = "731776";//代理账号id
+  this.api_key = "Y9FdFUagSQO2SuShpTGFyDnDnZrhgpQX";//api_key
 
-  this.customer_id = "";//客户id
+  this.customer_id = "18461745";//客户id
 
-  this.common_contact_id = "";//通用联系人id
-  this.eu_contact_id = "";//eu联系人id
+  this.common_contact_id = "76700781";//通用联系人id
+  this.eu_contact_id = "77037168";//eu联系人id
 }
 module.exports = User;
--- request.js
+++ request.js
@@ -1,28 +1,24 @@
 const request = require('request');
 const fs = require('fs');
-
 var userConfig = require('./config.js');
 const user = new userConfig;
 var domains = require('./domains.js')();
-
 const utils = require('./utils.js');
-
 let domainsArr = domains.domains;
 let nsArr = domains.ns;
-
 let timer = null;
 
 timer = setInterval(function () {
   console.log('---------' + domainsArr.length + '---------');
   domainsArr.forEach((element, index) => {
-    let contact_id = utils.suffixEu(element) ? user.eu_contact_id : user.common_contact_id;
+    const isEU = utils.suffixEu(element);
+    let contact_id = isEU ? user.eu_contact_id : user.common_contact_id;
+    const euStr = '&admin-contact-id=-1&tech-contact-id=-1&billing-contact-id=-1';
     const url = 'https://httpapi.com/api/domains/register.json?domain-name=' + element + '&auth-userid=' + user.auth_userid + '&api-key=' + user.api_key + '&years=1&ns=' + nsArr[0] + '&ns='
-      + nsArr[1] + '&customer-id=' + user.customer_id + '&reg-contact-id=' + contact_id + '&admin-contact-id=' + contact_id + '&tech-contact-id=' + contact_id + '&billing-contact-id=' + contact_id + '&invoice-option=PayInvoice&protect-privacy=true'
-
+      + nsArr[1] + '&customer-id=' + user.customer_id + '&reg-contact-id=' + contact_id + (isEU ? euStr : '') + '&admin-contact-id=' + contact_id + '&tech-contact-id=' + contact_id + '&billing-contact-id=' + contact_id + '&invoice-option=PayInvoice&protect-privacy=true'
     httprequest(url, index, element)
   });
 }, 5000);
-
 
 function httprequest(url, index, domain) {
   request({
@@ -34,10 +30,8 @@ function httprequest(url, index, domain) {
     }
   }, function (error, response, body) {
     console.log(domain);
-    console.log(body);
-    
+    // console.log(body);
     if (!error && response.statusCode == 200) {
-      //抢成功了
       if (body.status === 'Success') {
         fs.writeFile('./success/success' + utils.fileDate() + '.txt', domain, { 'flag': 'a', encoding: 'utf-8' }, (err, data) => {
           if (err) { throw err };
