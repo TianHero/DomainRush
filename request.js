578f9a05fbfe347780868b6d2f806928278dde0d
zhaotian <1246127205@qq.com>
2018-06-05 08:42:04 +0800

    success内换行

--- request.js
+++ request.js
@@ -15,8 +15,8 @@ timer = setInterval(function () {
     let contact_id = isEU ? user.eu_contact_id : user.common_contact_id;
     const euStr = '&admin-contact-id=-1&tech-contact-id=-1&billing-contact-id=-1';
     const url = 'https://httpapi.com/api/domains/register.json?domain-name=' + element + '&auth-userid=' + user.auth_userid + '&api-key=' + user.api_key + '&years=1&ns=' + nsArr[0] + '&ns='
-      + nsArr[1] + '&customer-id=' + user.customer_id + '&reg-contact-id=' + contact_id + (isEU ? euStr : '') + '&admin-contact-id=' + contact_id + '&tech-contact-id=' + contact_id + '&billing-contact-id=' + contact_id + '&invoice-option=PayInvoice&protect-privacy=true'
-    httprequest(url, index, element)
+      + nsArr[1] + '&customer-id=' + user.customer_id + '&reg-contact-id=' + contact_id + (isEU ? euStr : '') + '&admin-contact-id=' + contact_id + '&tech-contact-id=' + contact_id + '&billing-contact-id=' + contact_id + '&invoice-option=PayInvoice&protect-privacy=true';
+    httprequest(url, index, element);
   });
 }, 5000);
 
@@ -33,7 +33,7 @@ function httprequest(url, index, domain) {
     // console.log(body);
     if (!error && response.statusCode == 200) {
       if (body.status === 'Success') {
-        fs.writeFile('./success/success' + utils.fileDate() + '.txt', domain + '  ' +new Date().toISOString(), { 'flag': 'a', encoding: 'utf-8' }, (err, data) => {
+        fs.writeFile('./success/success' + utils.fileDate() + '.txt', domain + '--' + new Date().toISOString() + '\n', { 'flag': 'a', encoding: 'utf-8' }, (err, data) => {
           if (err) { throw err };
           domainsArr.splice(index, 1);
         });
