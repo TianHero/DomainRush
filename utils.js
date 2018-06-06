257f18f430014d42eb7a00e585ef96148b233d28
zhaotian <1246127205@qq.com>
2018-06-06 10:18:14 +0800

    utils

--- utils.js
+++ utils.js
@@ -3,13 +3,13 @@ function Utils() {
 }
 Utils.prototype = {
   constructor: Utils,
-  fileDate: function () {
+  fileDate: () => {
     let myDate = new Date();
     myDate.getMonth();
     myDate.getDate();
     return myDate.getMonth() + 1 + '-' + myDate.getDate();
   },
-  suffixEu: function (domain) {
+  suffixEu: domain => {
     let tmpArr = domain.split('.');
     const suf = tmpArr[tmpArr.length - 1];
     return suf === 'eu';
